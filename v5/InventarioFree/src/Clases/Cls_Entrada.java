7a2beb96cf74e6e646ed18e5ba94dd36ceb76e30
Dilan Sotomayor Salcedo <134469051+dilansoto13@users.noreply.github.com>
2023-10-26 19:49:27 -0600

    dilan2
    dilan2

--- v5/InventarioFree/src/Clases/Cls_Entrada.java
+++ v5/InventarioFree/src/Clases/Cls_Entrada.java
@@ -15,10 +15,11 @@ public class Cls_Entrada {
     private ResultSet RS;
     private final Conectar CN;
     private DefaultTableModel DT;
-    private final String SQL_INSERT_ENTRADA = "INSERT INTO entrada (ent_categoria, ent_pro_codigo, ent_fecha, ent_cantidad, cuerpo_merma, reja_merma, tapa_merma) values (?,?,?,?,?,?,?)";
-    private final String SQL_SELECT_ENTRADA = "SELECT ent_categoria, ent_fecha, ent_pro_codigo, pro_descripcion, nomproveedor, categoria , ent_cantidad, cuerpo_merma, reja_merma, tapa_merma FROM entrada INNER JOIN artículos ON ent_pro_codigo = pro_codigo";
+    private final String SQL_INSERT_ENTRADA = "INSERT INTO entrada (ent_categoria, ent_pro_codigo, ent_fecha, ent_cantidad, res_cuerpo, res_reja, res_tapa, cuerpo_merma, reja_merma, tapa_merma) values (?,?,?,?,?,?,?,?,?,?)";
+    private final String SQL_SELECT_ENTRADA = "SELECT ent_categoria, ent_fecha, ent_pro_codigo, pro_descripcion, nomproveedor, categoria , ent_cantidad, res_cuerpo, res_reja, res_tapa, cuerpo_merma, reja_merma, tapa_merma FROM entrada INNER JOIN artículos ON ent_pro_codigo = pro_codigo";
     private final String SQL_SELECT_CATEGORIA= "SELECT categoria FROM artículos WHERE pro_codigo = ?";
     private final String SQL_SELECT_ID= "SELECT MAX(ent_id) FROM entrada ";
+    
     public Cls_Entrada() {
         PS = null;
         CN = new Conectar();
@@ -38,7 +39,10 @@ public class Cls_Entrada {
         DT.addColumn("Descripción");
         DT.addColumn("Proveedor");
         DT.addColumn("Categoria");
-        DT.addColumn("Cantidad");
+        DT.addColumn("Tarimas");
+        DT.addColumn("Retiro de Cuerpos");
+        DT.addColumn("Retiro de Divisores");
+        DT.addColumn("Retiro de Tapa");
         DT.addColumn("Merma Cuerpo");
         DT.addColumn("Merma Divisor");
         DT.addColumn("Merma Tapa");
@@ -51,8 +55,8 @@ public class Cls_Entrada {
             setTitulosEntrada();
             PS = CN.getConnection().prepareStatement(SQL_SELECT_ENTRADA);
             RS = PS.executeQuery();
-            Object[] fila = new Object[10];////////////////////
-            while (RS.next()) {
+            Object[] fila = new Object[13];////////////////////
+            while (RS.next()) {                         
                 fila[0] = RS.getString(1);
                 fila[1] = RS.getDate(2);
                 fila[2] = RS.getString(3);
@@ -63,6 +67,9 @@ public class Cls_Entrada {
                 fila[7] = RS.getInt(8);
                 fila[8] = RS.getInt(9);
                 fila[9] = RS.getInt(10);
+                fila[10] = RS.getInt(11);
+                fila[11] = RS.getInt(12);
+                fila[12] = RS.getInt(13);
                 DT.addRow(fila);
 
             }
@@ -76,7 +83,7 @@ public class Cls_Entrada {
         return DT;
     }
     
-     public String generarFolio(String identificador, Date fecha) {
+    public String generarFolio(String identificador, Date fecha) {
         String folio = "";
         try {
             PS = CN.getConnection().prepareStatement(SQL_SELECT_CATEGORIA);
@@ -131,8 +138,8 @@ public class Cls_Entrada {
         }
         return id;
     }
-    
-    public int registrarEntrada(String categoria, String codigo, Date fecha, int cantidad,int cuerpomerma,int rejamerma, int tapamerma) {
+
+    public int registrarEntrada(String categoria, String codigo, Date fecha, int cantidad,int rescuerpo, int resreja, int restapa, int cuerpomerma,int rejamerma, int tapamerma) {
     int res = 0;
     try {
         PS = CN.getConnection().prepareStatement(SQL_INSERT_ENTRADA);
@@ -140,9 +147,12 @@ public class Cls_Entrada {
         PS.setString(2, codigo);
         PS.setDate(3, fecha);
         PS.setInt(4, cantidad);
-        PS.setInt(5, cuerpomerma);
-        PS.setInt(6, rejamerma);
-        PS.setInt(7, tapamerma);
+        PS.setInt(5, rescuerpo);
+        PS.setInt(6, resreja);
+        PS.setInt(7, restapa);
+        PS.setInt(8, cuerpomerma);
+        PS.setInt(9, rejamerma);
+        PS.setInt(10, tapamerma);
         res = PS.executeUpdate();
         if (res > 0) {
 
@@ -176,46 +186,82 @@ public class Cls_Entrada {
                             System.out.println("Update Inventario");
 
                             // Actualiza el cuerpo
-                            int res4 = 0;
-                            String UPDATE_ART_CUERPO = "UPDATE artículos SET cuerpo = cuerpo - ? WHERE pro_codigo = ?";
-                            PS = CN.getConnection().prepareStatement(UPDATE_ART_CUERPO);
-                            PS.setInt(1, cuerpomerma);  
-                            PS.setString(2, codigo);
-                            res4 = PS.executeUpdate();
+int totalCuerpo = rescuerpo + cuerpomerma;
+String UPDATE_ART_CUERPO = "UPDATE artículos SET cuerpo = cuerpo - ? WHERE pro_codigo = ?";
+PS = CN.getConnection().prepareStatement(UPDATE_ART_CUERPO);
+PS.setInt(1, totalCuerpo);  
+PS.setString(2, codigo);
+int res4 = PS.executeUpdate();
 
-                            if (res4 > 0) {
-                                System.out.println("Campo cuerpo de la tabla de artículos actualizada");
-                            } else {
-                                System.out.println("Error al actualizar el campo cuerpo de la tabla de artículos");
-                            }
+if (res4 > 0) {
+    System.out.println("Campo cuerpo de la tabla de artículos actualizada");
+} else {
+    System.out.println("Error al actualizar el campo cuerpo de la tabla de artículos");
+}
 
-                            // Actualiza la reja
-                            int res5 = 0;
-                            String UPDATE_ART_REJA = "UPDATE artículos SET reja = reja - ? WHERE pro_codigo = ?";
-                            PS = CN.getConnection().prepareStatement(UPDATE_ART_REJA);
-                            PS.setInt(1, rejamerma);  
-                            PS.setString(2, codigo);
-                            res5 = PS.executeUpdate();
+// Actualiza la reja
+int totalReja = resreja + rejamerma;
+String UPDATE_ART_REJA = "UPDATE artículos SET reja = reja - ? WHERE pro_codigo = ?";
+PS = CN.getConnection().prepareStatement(UPDATE_ART_REJA);
+PS.setInt(1, totalReja);  
+PS.setString(2, codigo);
+int res5 = PS.executeUpdate();
 
-                            if (res5 > 0) {
-                                System.out.println("Campo reja de la tabla de artículos actualizada");
-                            } else {
-                                System.out.println("Error al actualizar el campo reja de la tabla de artículos");
-                            }
+if (res5 > 0) {
+    System.out.println("Campo reja de la tabla de artículos actualizada");
+} else {
+    System.out.println("Error al actualizar el campo reja de la tabla de artículos");
+}
 
-                            // Actualiza la tapa
-                            int res6 = 0;
-                            String UPDATE_ART_TAPA = "UPDATE artículos SET tapa = tapa - ? WHERE pro_codigo = ?";
-                            PS = CN.getConnection().prepareStatement(UPDATE_ART_TAPA);
-                            PS.setInt(1, tapamerma);  
-                            PS.setString(2, codigo);
-                            res6 = PS.executeUpdate();
+// Actualiza la tapa
+int totalTapa = restapa + tapamerma;
+String UPDATE_ART_TAPA = "UPDATE artículos SET tapa = tapa - ? WHERE pro_codigo = ?";
+PS = CN.getConnection().prepareStatement(UPDATE_ART_TAPA);
+PS.setInt(1, totalTapa);  
+PS.setString(2, codigo);
+int res6 = PS.executeUpdate();
 
-                            if (res6 > 0) {
-                                System.out.println("Campo tapa de la tabla de artículos actualizada");
-                            } else {
-                                System.out.println("Error al actualizar el campo tapa de la tabla de artículos");
-                            }
+if (res6 > 0) {
+    System.out.println("Campo tapa de la tabla de artículos actualizada");
+} else {
+    System.out.println("Error al actualizar el campo tapa de la tabla de artículos");
+}
+                            
+                            // Después de actualizar las cantidades, consulta las cantidades actuales.
+String QUERY_PIEZAS_ACTUALES = "SELECT cuerpo, reja, tapa FROM artículos WHERE pro_codigo = ?";
+PS = CN.getConnection().prepareStatement(QUERY_PIEZAS_ACTUALES);
+PS.setString(1, codigo);
+RS = PS.executeQuery();
+
+int cuerposDisponibles = 0;
+int rejasDisponibles = 0;
+int tapasDisponibles = 0;
+
+if (RS.next()) {
+    cuerposDisponibles = RS.getInt("cuerpo");
+    rejasDisponibles = RS.getInt("reja");
+    tapasDisponibles = RS.getInt("tapa");
+}
+
+// Calcula cuántas cajas completas puedes formar con las piezas disponibles.
+int cajasCompletas = Math.min(Math.min(cuerposDisponibles, rejasDisponibles), tapasDisponibles);
+
+System.out.println("Número de cajas completas que se pueden formar: " + cajasCompletas);
+
+// Actualiza el número de cajas completas en la tabla "entradas".
+String UPDATE_ENTRADAS = "UPDATE entrada SET ent_cantidad = ? WHERE ent_pro_codigo = ?";
+PS = CN.getConnection().prepareStatement(UPDATE_ENTRADAS);
+PS.setInt(1, cajasCompletas);
+PS.setString(2, codigo);
+int resUpdate = PS.executeUpdate();
+
+if (resUpdate > 0) {
+    System.out.println("Número de cajas actualizado correctamente en la tabla de entradas.");
+} else {
+    System.out.println("Error al actualizar el número de cajas en la tabla de entradas.");
+}
+
+
                         }
                     }
 
